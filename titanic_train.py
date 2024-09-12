cd939e03d3ff0d576c794a20f40f3622b96ef2ca
Hazel Apondi <apondihazel@gmail.com>
2024-09-12 22:30:38 +0300

    Update titanic_train.py
    Check relationships between numerical variables using correlation.

--- titanic_train.py
+++ titanic_train.py
@@ -36,12 +36,20 @@ sns.countplot(x = 'Survived', hue = 'Pclass', data = train, palette = 'rainbow')
 
 #Age distribtion of passengers
 sns.distplot(train['Age'].dropna(), kde = False, color = 'darkred', bins = 40)
+plt.title('Distribution of Age')
+plt.show()
 
 #Countplot of Siblings/Spouses aboard
 sns.countplot(x = 'SibSp', data = train)
 
 #Fare distribution
 train['Fare'].hist(color = 'green', bins = 40, figsize = (8,4))
+
+#Correlation matrix
+plt.figure(figsize=(10, 8))
+sns.heatmap(train.corr(), annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Matrix')
+plt.show()
 
 #check the average age by passenger class using a boxplot of Age by Pclass
 plt.figure(figsize=(12,7))
@@ -67,38 +75,6 @@ train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis = 1)
 
 #check heat map to establish missing values
 sns.heatmap(train.isnull(), yticklabels = False, cbar = False, cmap = 'viridis')
-
-#Passengers that survived vs passengers that passed away
-print(train["Survived"].value_counts())
-
-#Passengers that survived vs passengers that passed away as proportions
-print(train["Survived"].value_counts(normalize=True))
-
-#Males that survived vs males that passed away
-print(train["Survived"][train["Sex"]=="male"].value_counts())
-
-#Females that survived vs Females that passed away
-print(train["Survived"][train["Sex"]=="female"].value_counts())
-
-#Normalized male survival
-print(train["Survived"][train["Sex"]=="male"].value_counts(normalize=True))
-
-#Normalized female survival
-print(train["Survived"][train["Sex"]=="female"].value_counts(normalize=True))
-
-#Another variable that could influence survival is age, since it is probable children were saved first
-#Create the column Child and assign to 'NaN'
-train["Child"] = float('NaN')
-
-#Assign 1 to passengers under 18, 0 to those 18 or older. Print the new column.
-train["Child"][train["Age"] < 18] = 1
-train["Child"][train["Age"] >= 18] = 0
-
-#Print normalized Survival Rates for passengers under 18
-print(train["Survived"][train["Child"] == 1].value_counts(normalize = True))
-
-#Print normalized Survival Rates for passengers 18 or older
-print(train["Survived"][train["Child"] == 0].value_counts(normalize = True))
 
 #drop the Cabin column and the rows with value NaN
 train.drop('Cabin', axis = 1, inplace = True)
